5916d8bc8eabcb00dddd7829a41bc238315aacd0
Aditya Harde <adityaharde05@gmail.com>
2024-08-23 12:02:31 +0530

    Made Changes

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -18,7 +18,7 @@ function TodoItem({ todo }) {
     return (
         <div
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
-                todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+                todo.isComplete ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
             }`}
         >
             <input
@@ -27,6 +27,7 @@ function TodoItem({ todo }) {
                 checked={todo.isComplete}
                 onChange={toggleCompleted}
             />
+            {/* Update the todo */}
             <input
                 type="text"
                 className={`border outline-none w-full bg-transparent rounded-lg ${
